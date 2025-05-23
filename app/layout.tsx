9fc4c29ee05996ced3a37baa004b3ae50aedd4d7
Bala <102295527+baala-xo@users.noreply.github.com>
2025-05-23 14:18:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'QR Cod Generator',
+  description: 'utility tool project',
+  generator: 'ba1a.xo',
 }
 
 export default function RootLayout({
